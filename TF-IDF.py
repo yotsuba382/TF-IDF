246e8dc91c5f938ada6563dd509ed6a39b2a4b60
RyotaFukuzawa <s1220241@u-aizu.ac.jp>
2018-05-05 22:48:16 +0900

    fix

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,19 +1,46 @@
 # coding: UTF-8
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-import sys
+import sys,csv
 
-argvs = sys.argv
-readFile = argvs[1]
+def readFile():
 
-np.set_printoptions(precision=2)
+    docs = []
+    np.set_printoptions(precision=2)
+    
+    argvs = sys.argv
+    file = argvs[1]
+    f = open(file,"r")
+    
+    first = f.readlines()
+    for line in first:
+        docs.append(line)
+    f.close()
 
-#docs = np.loadtxt(readFile, delimiter=" ")
-docs = np.array(["The Argentine\'s consistency in hitting the net is as astonishing due to the number of goals he has scored as it is due to the time in which he scored them. Barça's number 10 does just not tire of scoring, of winning of providing assists or of playing with pure joy. And as long as he keeps doing all that, we'll have Messi around for still a while to come. Messi's goal in the Copa del Rey final against Sevilla took the boy from Rosario on to 40 goals this ampaign, a tally he has sustained since 2009-10, Guardiola's second in the Barça dugout. And with five games remaining in the league (against Deportivo, Real Madrid, Villarrea, Levante and Real Sociedad), the Barça striker could yet increase his goal tally and beat the 41 goals he scored in 2013-14 and 2015-16. His record was in 2011-12 with a barnstorming 73 strikes in 60 games."])
+    #print(docs)
 
-print(docs)
- 
-vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u'(?u)\\b\\w+\\b')
-vecs = vectorizer.fit_transform(docs)
- 
-print(vecs.toarray())
+    return docs
+
+def makeCSV(X):
+    f = open('data.csv', 'w')
+
+    writer = csv.writer(f, lineterminator='\n')
+    #writer.writerow(list)
+    writer.writerows(X)
+
+    f.close()
+
+if __name__ == '__main__':
+
+    docs = readFile()
+
+    vectorizer = TfidfVectorizer(use_idf=True, token_pattern=u'(?u)\\b\\w+\\b')
+    vecs = vectorizer.fit_transform(docs)
+
+    X = vecs.toarray()
+    print(vecs.toarray())
+
+    #make Txt file
+    np.savetxt('input_data.txt', X,fmt="%.4f")
+
+    makeCSV(X)
